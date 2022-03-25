8a7d82b6ce355e94378ba2d0882a6a635d2f9425
Jaime-prog <A00571842@tec.mx>
2022-03-25 09:46:36 -0600

    modifica funcion tap para verificar si un espacio se encuentra ocupado

--- tictactoe.py
+++ tictactoe.py
@@ -44,6 +44,7 @@ def floor(value):
 
 state = {'player': 0}
 players = [drawx, drawo]
+places_on_grid= [" "," "," "," "," "," "," "," "," "]  # Array to verify if a place has already been taken
 
 def make_symbol(x,y,player):
     """Draw either the x or the o accordingly"""
@@ -55,6 +56,7 @@ def make_symbol(x,y,player):
 
 def tap(x, y):
     """Draw X or O in tapped square."""
+    global places_on_grid
     x = floor(x)
     y = floor(y)
     column=(x+205) 
@@ -62,7 +64,76 @@ def tap(x, y):
     square= column +row*3
     square=int(square)  # Set a specific value for each square
     player = state['player']
-    make_symbol(x,y,player)
+    
+    
+    if square == 422:
+        if places_on_grid[0]==" ":
+            places_on_grid[0]=1
+            make_symbol(x,y,player)
+            
+        elif places_on_grid[0]!="":
+            print("This space has already been used")
+    elif square == 555:
+        if places_on_grid[1]==" ":
+            places_on_grid[1]=1
+            make_symbol(x,y,player)
+           
+        else:
+            print("This space has already been used")
+    elif square == 688:
+        if places_on_grid[2]==" ":
+            places_on_grid[2]=1
+            make_symbol(x,y,player)
+            
+        else:
+            print("This space has already been used")
+    elif square == 821:
+        if places_on_grid[3]==" ":
+            places_on_grid[3]=821
+            make_symbol(x,y,player)
+            
+        else:
+            print("This space has already been used")
+
+    elif square == 954:
+        if places_on_grid[4]==" ":
+            places_on_grid[4]=1
+            make_symbol(x,y,player)
+            
+        else:
+            print("This space has already been used")
+
+    elif square == 1087:
+        if places_on_grid[5]==" ":
+            places_on_grid[5]=1
+            make_symbol(x,y,player)
+            
+        else:
+            print("This space has already been used")
+
+    elif square == 1220:
+        if places_on_grid[6]==" ":
+            places_on_grid[6]=1
+            make_symbol(x,y,player)
+           
+        else:
+            print("This space has already been used")
+
+    elif square == 1353:
+        if places_on_grid[7]==" ":
+            places_on_grid[7]=1
+            make_symbol(x,y,player)
+            
+        else:
+            print("This space has already been used")
+
+    elif square == 1486:
+        if places_on_grid[8]==" ":
+            places_on_grid[8]=1
+            make_symbol(x,y,player)
+            
+        else:
+            print("This space has already been used")
     
 
 
